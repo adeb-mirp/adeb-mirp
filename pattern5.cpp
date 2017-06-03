a17692413260a346771b628426e1fe5254798a5b
adeb-mirp <adeb-mirp@mit.edu>
2017-06-03 22:30:24 +0530

    Update pattern5.cpp

--- pattern5.cpp
+++ pattern5.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-int n;
+int n,sum=0;
 cout<<"Enter the number\n";
 cin>>n;
+for(int i=2;i<n;i++){
+        if(n%i==0)
+        sum+=i;
+    }
+    if(sum!=0)
+    cout<<"Not prime\n";
+    else
+    cout<<"Prime\n";
+    return 0;
+}
 
